5fb9e50e5e4ee9aa9cbca3d461e6d09ab90e7230
Ventery <user.emall=root@VM-0-12-centos>
2022-05-21 17:11:23 +0800

    bug fix Sat May 21 14:34:51 CST 2022

--- Base/ScreenRefresher.h
+++ Base/ScreenRefresher.h
@@ -53,20 +53,31 @@ namespace Global
         ProgressBarDemo(const std::string &title, const std::string &measure, long amount) : title_(title), measure_(measure), amount_(amount) {}
         std::string getBar(long number)
         {
+            static int sBarLength = 40;
             std::string result;
-            result += "+----------------------------------------+\n";
-            result += title_ + " : " + std::to_string(number) + " " + measure_ + " (" + std::to_string(((int)((number * 10000.0) / amount_)) / 100.0) + "%)\n";
+            int percentage = (int)((number * 10000.0) / amount_);
+            int first = percentage / 100;
+            int second = percentage % 100;
+            std::string titleLine = title_ + " : " + std::to_string(number) + " " + measure_ + " (" + std::to_string(first) + "." + std::to_string(second) + "%)";
+            int adjustLength = titleLine.length() - 2 > sBarLength ? titleLine.length() - 2 : sBarLength;
+            std::string crossLine = "+";
+            for (int i = 0; i < adjustLength; i++)
+                crossLine += "-";
+            crossLine += "+";
+
+            result += crossLine + "\n";
+            result += titleLine + "\n";
             result += "[";
-            int sum = (int)((number * 40.0) / amount_);
-            for (int i = 1; i <= 40; i++)
+            int sum = (int)((number * adjustLength * 1.0) / amount_);
+            for (int i = 1; i <= adjustLength; i++)
             {
                 if (i <= sum)
-                    result += "|";
+                    result += "-";
                 else
                     (result += " ");
             }
             result += "]\n";
-            result += "+----------------------------------------+\n";
+            result += crossLine + "\n";
 
             return result;
         }
